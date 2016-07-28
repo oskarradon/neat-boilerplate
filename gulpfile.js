815d611252a97da009b2d79677902fa4683796f1
Oskar <oskarradonkimball@gmail.com>
2016-07-28 02:13:41 -0700

    Get everything working!

--- gulpfile.js
+++ gulpfile.js
@@ -26,55 +26,51 @@ var reload                  = browserSync.reload;
 
 // Jade tasks
 gulp.task('jade', function() {
-  return gulp.src('*.jade')
-    .pipe(jade({ pretty: true }))
-    .pipe(gulp.dest(''))
+	return gulp.src('*.jade')
+	.pipe(jade({ pretty: true }))
+	.pipe(gulp.dest(''))
 });
 
 
 // CSS tasks
 gulp.task('css', function() {
-  return gulp.src('scss/*.scss')
-    // .pipe(sourcemaps.init())
-    .pipe(sass({  style: 'compressed', 
-                  noCache: true, 
-                  includePaths:
-                    [ 'scss/_partials/', 
-                      'scss/_vendor/', 
-                      bourbon, 
-                      neat ] }))
-    // .pipe(sourcemaps.write())
-    .pipe(autoprefixer())
-    .pipe(cssmin())
-    .pipe(gulp.dest('css'))
+	return gulp.src('scss/*.scss')
+	// .pipe(sourcemaps.init())
+	.pipe(sass({ style: 'compressed', 
+		noCache: true,
+		includePaths: [ bourbon, neat ] }))
+	// .pipe(sourcemaps.write())
+	.pipe(autoprefixer())
+	.pipe(cssmin())
+	.pipe(gulp.dest('css'))
 });
 
 // JS tasks
 gulp.task('cleanJs', function() {
-  del(['js/scripts.min.js']);
+	del(['js/scripts.min.js']);
 });
 
 gulp.task('js', function() {
-    return gulp.src('js/scripts.js')
-        .pipe(uglify())
-        .pipe(concat('scripts.min.js'))
-        .pipe(gulp.dest('js/'))
+	return gulp.src('js/scripts.js')
+	.pipe(uglify())
+	.pipe(concat('scripts.min.js'))
+	.pipe(gulp.dest('js/'))
 });
 
 // Watch files for changes
 gulp.task('watch', ['browser-sync'], function() {
-  gulp.watch('*.html', reload);
-  gulp.watch('scss/**/*', ['css', reload]);
-  gulp.watch('*.jade', ['jade']);
-  gulp.watch('scripts.js', ['cleanJs', 'js']);
+	gulp.watch('*.html', reload);
+	gulp.watch('scss/**/*', ['css', reload]);
+	gulp.watch('*.jade', ['jade']);
+	gulp.watch('scripts.js', ['cleanJs', 'js']);
 });
 
 gulp.task('browser-sync', function() {
-  browserSync.init(['css/*', 'js/*'], {
-    server: {
-      baseDir: ""
-    }
-  });
+	browserSync.init(['css/*', 'js/*'], {
+		server: {
+			baseDir: ""
+		}
+	});
 });
 
 // Default task
